d8c5ef4d46c67a6c70e9dcb80f6da0376431ebec
Lucasteisouza <lucasteisouza@gmail.com>
2023-05-06 15:32:57 -0300

    feat: cria a classe Character que permite a criação de personagens base, usando como padrão um Elf Mage

---
+++ src/Character.ts
@@ -0,0 +1,93 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = Math.floor(Math.random() * 10) + 1;
+    this._strength = Math.floor(Math.random() * 10) + 1;
+    this._defense = Math.floor(Math.random() * 10) + 1;
+    this._race = new Elf(name, this._dexterity);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._archetype = new Mage(name);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: Math.floor(Math.random() * 10) + 1,
+    };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (damage <= 0) {
+      this._lifePoints -= 1;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  public attack(enemy: Fighter): void {
+    const attackPoints = this._strength;
+    enemy.receiveDamage(attackPoints);
+  }
+
+  public levelUp(): void {
+    this._maxLifePoints += Math.floor(Math.random() * 10) + 1;
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+    this._strength += Math.floor(Math.random() * 10) + 1;
+    this._dexterity += Math.floor(Math.random() * 10) + 1;
+    this._defense += Math.floor(Math.random() * 10) + 1;
+    this._energy.amount = 10;
+  }
+}
+
+export default Character;
